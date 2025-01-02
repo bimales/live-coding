bcd7b00eb26d2d3e822c5723bafcf54208ae5f20
Bimales Mandal <mail2bimales@gmail.com>
2025-01-02 15:22:40 +0530

    base live coding project

---
+++ src/test/java/org/live/coding/LiveCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.live.coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
